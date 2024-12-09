1cacaba187163092c960918d077edad247bc78a8
amaypip481 <179745277+amaypip481@users.noreply.github.com>
2024-12-10 00:17:09 +0530

    change design for add button & book detail form

--- Library.js
+++ Library.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+// Book Object 
+
 function Book(title, author, genre, rating, description, id) {
     return {
         Title: title,
@@ -11,6 +13,9 @@ function Book(title, author, genre, rating, description, id) {
         isAddedToDom: false
     };
 }
+
+// Create HTML DOM with details from the form inputs by the user
+
 
 function createBookCard(htmlDom) {
     myLibrary.forEach((cardDetail) => {
@@ -33,9 +38,16 @@ function createBookCard(htmlDom) {
                             <header>Genre: </header>
                             <p></p>
                         </div>
-                        <div class="Rating imgSideContent">
+                        <div class="Rating imgSideContent" >
                             <header>Rating: </header>
-                            <p></p>
+                             <select name="rating" class="ratingselector">
+                            <option value="">--Please Choose--</option>
+                            <option value="one">1</option>
+                            <option value="Two">2</option>
+                            <option value="Three">3</option>
+                            <option value="Four">4</option>
+                            <option value="Five">5</option>
+                        </select>
                         </div>
                     </div>
                     <div class="cardItemDescription">
@@ -50,7 +62,6 @@ function createBookCard(htmlDom) {
             cardItems.querySelector(".Title").querySelector("p").textContent += cardDetail.Title;
             cardItems.querySelector(".Author").querySelector("p").textContent += cardDetail.Author;
             cardItems.querySelector(".Genre").querySelector("p").textContent += cardDetail.Genre;
-            cardItems.querySelector(".Rating").querySelector("p").textContent += cardDetail.Rating + "/10";
             cardItems.querySelector(".cardItemDescription").querySelector("p").textContent += cardDetail.Description;
             cardItems.querySelector(".cardItemButtons").querySelector(".read").addEventListener("click", isread);
             cardItems.querySelector(".cardItemButtons").querySelector(".readLater").addEventListener("click", readForLater);
@@ -63,13 +74,19 @@ function createBookCard(htmlDom) {
 
 }
 
+
+// Seems unecessary - let's keep it for now
+
 function displayBookToLibrary() {
     errorMessageReset();
     let cardItemsParent = document.querySelector("#content");
     let cardItems = createBookCard(cardItemsParent);
-    document.querySelector("dialog").close();
+    document.querySelector("#addBookDialog").close();
 
 }
+
+
+//  call back for submit click event on the form 
 
 function addBookToLibrary(e) {
     e.preventDefault();
@@ -92,38 +109,54 @@ function addBookToLibrary(e) {
         Genre.nextElementSibling.style.display = "block";
         return;
     }
-    let Rating = addBookForm.querySelector("#formRating");
-    if(Rating.value !== '' && (Rating.value >10 || Rating.value <1)) {
-        Rating.style.border = "0.1rem solid red";
-        Rating.nextElementSibling.style.display = "block";
-        return;
-    }
       
     let Description = addBookForm.querySelector("#formDescription");
     let id = "cardItem" + myLibrary.length + 1;
-    let newBook = Book(Title.value, Author.value, Genre.value, Rating.value, Description.value, id);
+    let newBook = Book(Title.value, Author.value, Genre.value, "", Description.value, id);
     myLibrary.push(newBook);
     document.querySelector("#addBooksForm").reset();
     displayBookToLibrary();
 }
 
+// Add eventListeners
+
 document.getElementById("addBooks").addEventListener("click", () => {
-    document.querySelector("dialog").showModal();
+    document.querySelector("#addBookDialog").showModal();
 });
 let formDetails = document.querySelector("#addBooksForm");
 let submitButton = formDetails.querySelector(".formButtonDiv").querySelector("button");
 
 submitButton.addEventListener("click", addBookToLibrary);
-
 document.querySelector("#formClose").addEventListener("click",closeForm);
 
+function selectorChangeEvent(e){
+    let parent = e.target.parentElement.parentElement.parentElement;
+    myLibrary.forEach((book)=>{
+        if(book.id === parent.id){
+            book.Rating = e.target.value;
+        }
+    });
+
+}
 
 function isread(e) {
+    let ratingSelectorValue = document.querySelector(".ratingselector");
+    ratingSelectorValue.addEventListener("change", selectorChangeEvent);
+    let parent = e.target.parentElement.parentElement;
     if (e.target.style.backgroundColor === "rgb(4, 139, 42)")
+    {
         e.target.style.backgroundColor = "rgba(255, 255, 255, 0.1)";
+        parent.querySelector(".Rating").style.display = "none";
+    }
     else
+    {
         e.target.style.backgroundColor = "rgb(4, 139, 42)";
+        parent.querySelector(".Rating").style.display = "flex";
+    }
+
 }
+
+// Read button click eventListener
 
 function readForLater(e) {
     if (e.target.style.backgroundColor === "rgb(194, 35, 91)")
@@ -131,6 +164,8 @@ function readForLater(e) {
     else
         e.target.style.backgroundColor = "rgb(194, 35, 91)";
 }
+
+// Delete button click eventListener
 
 function deleteButton(e) {
     let parent = e.target.parentElement.parentElement;
@@ -142,25 +177,29 @@ function deleteButton(e) {
     });
 }
 
+// Close button click eventListener
+
 function closeForm(e) {
     e.preventDefault();
     errorMessageReset();
     document.querySelector("#addBooksForm").reset();
-    document.querySelector("dialog").close();
+    document.querySelector("#addBookDialog").close();
 }
+
+
+//  Reset the error message in the form
 
 function errorMessageReset(){
     let addBookForm = document.querySelector("#addBooksForm");
     let Title = addBookForm.querySelector("#formTitle");
     let Author = addBookForm.querySelector("#formAuthor");
     let Genre = addBookForm.querySelector("#formGenre");
-    let Rating = addBookForm.querySelector("#formRating");
+   
     Title.style.border = "";
     Title.nextElementSibling.style.display = "none";
     Author.style.border = "";
     Author.nextElementSibling.style.display = "none";
     Genre.style.border = "";
     Genre.nextElementSibling.style.display = "none";
-    Rating.style.border = "";
-    Rating.nextElementSibling.style.display = "none";
+
 }
